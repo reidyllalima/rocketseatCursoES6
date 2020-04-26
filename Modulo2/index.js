061080b0787c1d99e23f1e9b4feb229a54a8a670
Reidylla Lima <reidylla.lima@pop-pe.rnp.br>
2020-04-26 18:35:23 -0300

    alterandoDefault

--- Modulo2/index.js
+++ Modulo2/index.js
@@ -21,4 +21,4 @@ import { idade } from './function';
 console.log(idade);
 
 //1.3
-import defaultUsuario, { idade as IdadeUsuario } from './function';
+import Usuario, { idade as IdadeUsuario } from './function';
