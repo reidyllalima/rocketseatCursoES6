d144191dcd8f17bd56f546725f15230ab3d8f05a
Reidylla Lima <reidylla.lima@pop-pe.rnp.br>
2020-04-26 18:32:55 -0300

    modulo2-Webpack

---
+++ Modulo2/index.js
@@ -0,0 +1,24 @@
+/*
+1.1
+Agora em seu arquivo principal import apenas a classe Usuario renomeando-a para ClasseUsuario
+e chame a funão info();
+1.2
+Em seu arquivo principal importe a variável de idade e exiba a mesma em tela;
+1.3
+Em seu arquivo principal importe tanto a classe quanto a variável idade e renomeie a variável idade
+para IdadeUsuario.
+*/
+
+
+//1.1
+import ClasseUsuario from './function';
+
+ClasseUsuario.info();
+
+//1.2
+
+import { idade } from './function';
+console.log(idade);
+
+//1.3
+import defaultUsuario, { idade as IdadeUsuario } from './function';
